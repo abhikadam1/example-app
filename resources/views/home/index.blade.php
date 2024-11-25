9ed62c6fd7f64448e20efcd28a9e8fc78dcd56d4
Abhi Kadam <kadamabhishek3188@gmail.com>
2024-11-26 00:03:39 +0530

    new files

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,19 @@
 <x-header data="Header Title"/>
-<h1>{{ $message }}</h1>
+@include('inner')
+<h1>{{ $data['message'] }}</h1>
+
+@foreach ($data as $value )
+<div class="row">
+    <div class="col-md-4">
+    <h3><i><u>{{$value}}</u></i></h3>
+    </div>
+</div>
+
+@endforeach
 <!-- <h1><a href="{{ url('/') }}">{{ url('/') }}</a></h1> -->
+
+<script>
+    let data = @json($data);
+    console.log(data, " data instanceof");
+    
+</script>
---
+++ resources/views/inner.blade.php
@@ -0,0 +1,29 @@
+<header>
+    Header View
+    <div>
+        Header 1
+    </div>
+    <div>
+        Header 1
+    </div>
+    <div>
+        Header 1
+    </div>
+    <div>
+        Header 1
+    </div>
+</header>
+
+<style>
+    header {
+        background-color: skyblue;
+        padding: 20px;
+        display: flex;
+    }
+    header div{
+        background-color: lightblue;
+        width: 100px;
+        display: flex;
+        margin: auto;
+    }
+</style>
